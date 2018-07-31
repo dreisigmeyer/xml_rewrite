b6d07d7be834adfb0f1ec03817d621c049d28a13
David W Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-07-31 07:45:57 -0400

    README and directory for input files

---
+++ README.md
@@ -0,0 +1,10 @@
+## Getting the data
+The included script _get\_uspto\_data.sh_ will download all the required zip files from the USPTO website.
+
+
+## Setting up the Python environment
+A standard Anaconda Python (https://www.anaconda.com/download/) install was used to develop the code.
+The code was developed using Python 3.6.
+
+
+
--- get_uspto_data.sh
+++ get_uspto_data.sh
@@ -2,18 +2,9 @@
 
 for YEAR in {2002..2017}
 do
-    COUNT=`ls python_validation/inData/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
+    COUNT=`ls rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
     while [ "$COUNT" -lt 52 ]; do
-        `wget -q -r -l1 -nd -P ./python_validation/inData/ --no-parent -A '*_wk*.zip' "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
-        COUNT=`ls python_validation/inData/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
+        `wget -q -r -l1 -nd -P ./rewriter/raw_xml_files/ --no-parent -A '*_wk*.zip' "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
+        COUNT=`ls rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
     done
 done
-
-#for YEAR in {1976..2001}
-#do
-#    COUNT=`ls GBD_1976_2001_dat_to_xml/inData/"$YEAR".zip 2> /dev/null | wc -l`
-#    while [ "$COUNT" -lt 1 ]; do
-#        `wget -q -r -l1 -nd -P ./GBD_1976_2001_dat_to_xml/inData/ --no-parent -A "$YEAR.zip" "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
-#        COUNT=`ls GBD_1976_2001_dat_to_xml/inData/"$YEAR".zip 2> /dev/null | wc -l`
-#    done
-#done
