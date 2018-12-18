93d06175a6d221ed075918e4506b9d926f6679f9
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-12-18 10:22:46 -0500

    Using full path to input files

--- rewriter/__main__.py
+++ rewriter/__main__.py
@@ -1,13 +1,15 @@
 import glob
 from multiprocessing import Process
+import os
 import random
 from rewriter.rewriter import process_files
 from shared_python_code.utility_functons import split_seq
 import sys
 
+THIS_DIR = os.path.dirname(__file__)
 
 number_of_processes = int(sys.argv[1])
-in_dir_path = 'rewriter/raw_xml_files/'
+in_dir_path = THIS_DIR + '/raw_xml_files/'
 in_directories = glob.glob(in_dir_path + '*')
 random.shuffle(in_directories)  # Newer years have more granted patents
 directories_list = split_seq(in_directories, number_of_processes)
