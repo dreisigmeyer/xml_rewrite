cb8c6d0b2f3fee71312134c1bc1e037c8c7bcec2
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-12-18 12:14:02 -0500

    updating count

--- get_uspto_data.sh
+++ get_uspto_data.sh
@@ -5,9 +5,9 @@
 PATH_TO_XML=`dirname $(readlink -f $0)`
 for YEAR in {2002..2017}
 do
-    COUNT=`ls rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
+    COUNT=`ls "$PATH_TO_XML"/rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
     while [ "$COUNT" -lt 52 ]; do
         `wget -q -r -l1 -nd -P "$PATH_TO_XML"/rewriter/raw_xml_files/ --no-parent -A '*_wk*.zip' "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
-        COUNT=`ls rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
+        COUNT=`ls "$PATH_TO_XML"/rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
     done
 done
