83340f7b2b3bb6b82fbb09bc44b89c7c0947333f
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-12-18 11:31:13 -0500

    corrected bash variable error

--- get_uspto_data.sh
+++ get_uspto_data.sh
@@ -7,7 +7,7 @@ for YEAR in {2002..2017}
 do
     COUNT=`ls rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
     while [ "$COUNT" -lt 52 ]; do
-        `wget -q -r -l1 -nd -P "PATH_TO_XML"/rewriter/raw_xml_files/ --no-parent -A '*_wk*.zip' "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
+        `wget -q -r -l1 -nd -P "$PATH_TO_XML"/rewriter/raw_xml_files/ --no-parent -A '*_wk*.zip' "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
         COUNT=`ls rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
     done
 done
