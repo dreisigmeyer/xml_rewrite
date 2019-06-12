a843cde9a654526f0b124190e7bf4cea37c65ab4
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2019-06-12 09:41:06 -0400

    Modified wget to USPTO

--- get_uspto_data.sh
+++ get_uspto_data.sh
@@ -3,10 +3,13 @@
 # This will download the data files from the USPTO
 
 PATH_TO_XML=`dirname $(readlink -f $0)`
-for YEAR in {2002..2019}
+XML_END_YEAR=$1
+
+for (( YEAR=2002; YEAR<=$XML_END_YEAR; YEAR++ ))
 do
+    NUM_FILES=`wget -qO- --no-parent -A '*_wk*.zip' "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/" | grep -E "*_wk[0-9]{2}.zip" | wc -l`
     COUNT=`ls "$PATH_TO_XML"/rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
-    while [ "$COUNT" -lt 52 ]; do
+    while [ "$COUNT" -lt "$NUM_FILES" ]; do
         `wget -q -r -l1 -nd -P "$PATH_TO_XML"/rewriter/raw_xml_files/ --no-parent -A '*_wk*.zip' "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
         COUNT=`ls "$PATH_TO_XML"/rewriter/raw_xml_files/*pgb"$YEAR"*_wk*.zip 2> /dev/null | wc -l`
     done
